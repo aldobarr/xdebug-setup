e4d23d86f79db79b106370c3e4d0cf1b3085c771
aldobarr <hadesflames@live.com>
2022-05-07 06:01:15 -0400

    Update xdebug_setup.sh

--- xdebug_setup.sh
+++ xdebug_setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+NO_PROXY=0
 VERSION="3.0.3"
-usage(){
+function usage() {
 	echo "Usage $0
 	[-v version]
+	[-n no dbgpProxy]
 	[-h help]"
 }
 
-while getopts :hv opt; do
+while getopts hv:n opt; do
 	case $opt in 
 		h) usage; exit ;;
-		v) VERSION=$2 ;;
-	\?) ;;
+		v) VERSION=$OPTARG ;;
+		n) NO_PROXY=1 ;;
 	esac
 done
 
@@ -31,6 +33,12 @@ if ! command -v wget &> /dev/null
 then
 	echo "wget could not be found"
 	exit
+fi
+
+INSTALL_PROGRAMS="php-dev autoconf automake"
+if [ $NO_PROXY -ne 1 ]
+then
+	INSTALL_PROGRAMS="$INSTALL_PROGRAMS supervisor"
 fi
 
 PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;")
@@ -56,7 +64,7 @@ then
 fi
 
 apt update
-apt install -y php-dev autoconf automake supervisor
+apt install -y $INSTALL_PROGRAMS
 
 if [ ! -d /tmp/xdebug_install ]
 then
@@ -104,36 +112,39 @@ touch /etc/tmpfiles.d/xdebug-cleanup.conf
 echo "d /tmp/xdebug 0777 root root 10m" >> /etc/tmpfiles.d/xdebug-cleanup.conf
 chmod 644 /etc/tmpfiles.d/xdebug-cleanup.conf
 
-if [ -f /usr/bin/dbgpProxy ]
-then
-	rm -f /usr/bin/dbgpProxy
-fi
-
 phpenmod xdebug
 systemctl restart apache2
 
-wget https://xdebug.org/files/binaries/dbgpProxy
-chmod +x dbgpProxy
-mv dbgpProxy /usr/bin/
-
-SUPERVISOR_CONF="[program:dbgpProxy]
-command=/usr/bin/dbgpProxy -s 127.0.0.1:9003
-autostart=true
-autorestart=true
-stderr_logfile=/var/log/dbgpProxy.error
-stdout_logfile=/var/log/dbgpProxy.log"
-
-if [ -f /etc/supervisor/conf.d/dbgpProxy.conf ]
+if [ $NO_PROXY -ne 1 ]
 then
-	rm -f /etc/supervisor/conf.d/dbgpProxy.conf
+	if [ -f /usr/bin/dbgpProxy ]
+	then
+		rm -f /usr/bin/dbgpProxy
+	fi
+
+	wget https://xdebug.org/files/binaries/dbgpProxy
+	chmod +x dbgpProxy
+	mv dbgpProxy /usr/bin/
+
+	SUPERVISOR_CONF="[program:dbgpProxy]
+	command=/usr/bin/dbgpProxy -s 127.0.0.1:9003
+	autostart=true
+	autorestart=true
+	stderr_logfile=/var/log/dbgpProxy.error
+	stdout_logfile=/var/log/dbgpProxy.log"
+
+	if [ -f /etc/supervisor/conf.d/dbgpProxy.conf ]
+	then
+		rm -f /etc/supervisor/conf.d/dbgpProxy.conf
+	fi
+
+	touch /etc/supervisor/conf.d/dbgpProxy.conf
+	echo "$SUPERVISOR_CONF" >> /etc/supervisor/conf.d/dbgpProxy.conf
+	chmod 644 /etc/supervisor/conf.d/dbgpProxy.conf
+
+	supervisorctl reread
+	supervisorctl update
 fi
-
-touch /etc/supervisor/conf.d/dbgpProxy.conf
-echo "$SUPERVISOR_CONF" >> /etc/supervisor/conf.d/dbgpProxy.conf
-chmod 644 /etc/supervisor/conf.d/dbgpProxy.conf
-
-supervisorctl reread
-supervisorctl update
 
 cd ../
 rm -rf /tmp/xdebug_install
